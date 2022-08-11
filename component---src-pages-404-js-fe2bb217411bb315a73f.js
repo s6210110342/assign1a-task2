4c743103601ab25059ed2c23edf60f3d046beeb3
ศิขรินทร์ สามงามทอง <ploysikarin11@gmail.com>
2022-08-12 00:05:47 +0700

    Updates

---
+++ component---src-pages-404-js-fe2bb217411bb315a73f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5=self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5||[]).push([[883],{429:function(t,e,a){a.r(e);var r=a(7294),n=a(6301),s=a(262);e.default=function(){return r.createElement(n.Z,null,r.createElement(s.Z,{title:"404: Not found"}),r.createElement("div",{className:"container text-center my-5"},r.createElement("h1",null,"404: Not Found"),r.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fe2bb217411bb315a73f.js.map
